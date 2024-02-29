557a3f5a18130136f09c0d6d6ad7b5a7d4fc9b66
Wey <weyCC81@users.noreply.github.com>
2024-02-29 11:30:50 +0100

    Update graph.ps1
    Prevent guests from being added to a specific Microsoft 365 group or Microsoft Teams team

--- Instructions/Links (Addition)/graph.ps1
+++ Instructions/Links (Addition)/graph.ps1
@@ -138,3 +138,13 @@ Update-MgBetaDirectorySetting -DirectorySettingId $Setting.Id -BodyParameter $pa
 #Disconnect-AzureAD
 Disconnect-MgGraph
 
+
+# ------Addition
+# Prevent guests from being added to a specific Microsoft 365 group or Microsoft Teams team
+
+# https://learn.microsoft.com/en-us/microsoft-365/solutions/per-group-guest-access?view=o365-worldwide
+
+(Invoke-GraphRequest -Uri https://graph.microsoft.com/beta/Groups/$groupId/settings -Method GET) | ConvertTo-Json | ConvertFrom-Json | fl Value
+(Get-MgBetaGroupSetting -GroupId $groupId | Where-Object { $_.DisplayName -eq "Group.Unified.Guest"}).Values
+# name = "AllowToAddGuests" value = "false"
+
